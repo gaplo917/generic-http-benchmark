0aca8e4110cb7e19724c0e23734a29ede4dda024
Gary Lo <logap@me.com>
2022-11-08 13:02:39 +0800

    add java 19 structured concurrency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,9 @@ tasks.test { useJUnitPlatform() }
 
 tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
 
-tasks.withType<JavaExec> { environment("JAVA_TOOL_OPTIONS", "--enable-preview --add-modules=jdk.incubator.concurrent") }
+tasks.withType<JavaExec> {
+  environment("JAVA_TOOL_OPTIONS", "--enable-preview --add-modules=jdk.incubator.concurrent")
+}
 
 application { mainClass.set("MainKt") }
 
