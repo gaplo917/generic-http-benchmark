775de4bc3931334d7123d85c3bae7940c692cc71
Gary Lo <logap@me.com>
2022-11-21 17:30:03 +0800

    remove prometheus grafana in benchmark script

--- e2e-gatling-benchmark.sh
+++ e2e-gatling-benchmark.sh
@@ -8,7 +8,7 @@ for env in ./config/* ; do
   # Add date
   echo "Benchmark $ENV_FILE at $(date -u)" | tee -a $RESULT;
 
-  docker compose --env-file $ENV_FILE up -d benchmark-target prometheus grafana;
+  docker compose --env-file $ENV_FILE up -d benchmark-target;
   docker compose --env-file $ENV_FILE up gatling-runner | tee -a $RESULT;
   docker compose --env-file $ENV_FILE down;
 done
