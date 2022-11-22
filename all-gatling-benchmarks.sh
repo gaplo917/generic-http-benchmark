1809992c326645d20d559bd9f12e859a293be0aa
Gary Lo <logap@me.com>
2022-11-22 18:19:46 +0800

    generate upload folder after test

--- all-gatling-benchmarks.sh
+++ all-gatling-benchmarks.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 RESULT_PATH="logs";
-DIR="$RESULT_PATH/$(date +%Y%m%d%H%M%S)";
-
+UPLOAD_PATH="upload";
+DATE_TIMESTAMP=$(date +%Y%m%d%H%M%S)
+DIR="$RESULT_PATH/$DATE_TIMESTAMP";
+UPLOAD_DIR="$UPLOAD_PATH/$DATE_TIMESTAMP";
 mkdir -p $DIR;
+mkdir -p $UPLOAD_DIR;
 
-for env in ./config/* ; do
+for env in "./config/ktor.env" ; do
   ENV_FILE=$env;
   FILE_NAME="$(basename -s .env $env)";
   RESULT="$DIR/$FILE_NAME.txt";
@@ -17,5 +20,10 @@ for env in ./config/* ; do
   docker compose --env-file $ENV_FILE build && \
   docker compose --env-file $ENV_FILE up -d benchmark-target && \
   docker compose --env-file $ENV_FILE up gatling-runner | tee -a $RESULT && \
-  docker compose --env-file $ENV_FILE down;
+  docker compose --env-file $ENV_FILE down && \
+  LINE=$(cat $RESULT | grep "Please open the following file: file:///usr/src/app/build/reports/gatling/") && \
+  FIRST_PASS="${LINE/gatling-runner  | Please open the following file: file:\/\/\/usr\/src\/app\/build\/reports\/gatling\//}" && \
+  FOLDER="${FIRST_PASS/\/index.html/}" && \
+  rsync -av --exclude='*.log' "./reports/gatling/$FOLDER/" "./$UPLOAD_DIR/$FILE_NAME";
+
 done
